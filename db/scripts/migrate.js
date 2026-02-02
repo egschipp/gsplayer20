4891b3a0ebbc9890c53ae758f7a3edf80eb1ab6b
George Schippers <George@Schippers-Online.nl>
2026-02-02 18:15:07 +0100

    Run SQL migration without drizzle migrator

--- db/scripts/migrate.js
+++ db/scripts/migrate.js
@@ -1,14 +1,14 @@
+const fs = require("fs");
+const path = require("path");
 const Database = require("better-sqlite3");
-const { drizzle } = require("drizzle-orm/better-sqlite3");
-const { migrate } = require("drizzle-orm/better-sqlite3/migrator");
 
 const dbPath = process.env.DB_PATH || "/data/gsplayer.sqlite";
 const sqlite = new Database(dbPath);
 sqlite.pragma("journal_mode = WAL");
 sqlite.pragma("foreign_keys = ON");
 
-const db = drizzle(sqlite);
-
-migrate(db, { migrationsFolder: "db/migrations" });
+const migrationPath = path.join(__dirname, "..", "migrations", "0001_init.sql");
+const sql = fs.readFileSync(migrationPath, "utf8");
+sqlite.exec(sql);
 
 console.log("Migrations applied");
